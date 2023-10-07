b7828f1a816a643a24a15e9fb71137585a459a63
Abhay Upadhyay <abhay281103@gmail.com>
2023-10-07 23:37:22 +0530

    tableland utils final

--- src/app/utils/tablelandUtils.js
+++ src/app/utils/tablelandUtils.js
@@ -23,30 +23,71 @@ export const createTable = async (tableName, fields) => {
   const { meta: create } = await db
     .prepare(`CREATE TABLE ${tableName} (${fields});`)
     .run()
-
-  console.log(create.txn.name)
+  console.log(create.txn.transactionHash)
   return create.txn.name
 }
 
+const user_table = 'users_80001_7679'
+const user_models_table = 'user_models_80001_7684'
+
 //@todo: get table name first
-export const add_user = async (
-  id,
-  name,
-  lastLoggedIn,
-  createdAt,
-  updatedAt
-) => {
-  const user_table = ''
+export const add_user = async (id, name) => {
   const signer = get_pk_walletClient_mumbai()
   const db = new Database({ signer })
-  const { meta: insert } = await db
+  let time = new Date().toLocaleString('en-GB', { timeZone: 'UTC' })
+  const {
+    meta: insert,
+    success,
+    error
+  } = await db
     .prepare(
       `INSERT INTO ${user_table} (id, name, lastLoggedIn, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?);`
     )
-    .bind(id, name, lastLoggedIn, createdAt, updatedAt)
+    .bind(id, name, time, time, time)
     .run()
   const hash = insert.txn.transactionHash
+  if (!success) {
+    new Error(error)
+  }
+  await publicClient_mumbai.waitForTransactionReceipt({ hash })
+  console.log('Added to users table')
+}
+
+export const add_user_models = async (userId, name, cid) => {
+  const signer = get_pk_walletClient_mumbai()
+  const db = new Database({ signer })
+  let time = new Date().toLocaleString('en-GB', { timeZone: 'UTC' })
+  const {
+    meta: insert,
+    success,
+    error
+  } = await db
+    .prepare(
+      `INSERT INTO ${user_models_table} (userId, name, cid , createdAt, updatedAt) VALUES ( ?, ?, ?, ?, ?);`
+    )
+    .bind(userId, name, cid, time, time)
+    .run()
+  if (!success) {
+    new Error(error)
+  }
+  const hash = insert.txn.transactionHash
   console.log(hash)
-  await publicClient.waitForTransactionReceipt({ hash })
-  console.log('Added to dao data')
+  await publicClient_mumbai.waitForTransactionReceipt({ hash })
+  console.log('Added to users table')
+}
+
+export const user_logged_in = async userId => {
+  const signer = get_pk_walletClient_mumbai()
+  const db = new Database({ signer })
+  let time = new Date().toLocaleString('en-GB', { timeZone: 'UTC' })
+  const stmt = await db
+    .prepare(`UPDATE ${user_table} SET lastLoggedIn=${time} WHERE id=${userId}`)
+    .run()
+
+  if (!stmt.success) {
+    new Error(stmt.error)
+  }
+  const hash = stmt.meta.txn.transactionHash
+  await publicClient_mumbai.waitForTransactionReceipt({ hash })
+  console.log('Updated User Login Data')
 }
