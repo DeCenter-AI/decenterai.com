ca1a57942e0495a0de7499cdbcf6d536a9c70858
sheriff Oladimeji <dimejiademola5@gmail.com>
2024-01-18 22:05:44 +0100

    fix: update presale page
    feat: create custom dropdown

--- src/app/presale/page.tsx
+++ src/app/presale/page.tsx
@@ -6,10 +6,33 @@ import logo from 'public/logo-big.png'
 import { LuArrowBigDown } from 'react-icons/lu'
 import usdt from 'public/usdt.png'
 import icon from 'public/small.png'
-
+import { FaBtc } from 'react-icons/fa'
+import { PiCaretDown } from 'react-icons/pi'
+import { AnyCnameRecord } from 'dns'
 const PresalePage = () => {
   const price = 0.022
+ const [isDropdownOpen, setDropdownOpen] = useState<boolean>(false)
+ const [selectedToken, setSelectedToken] = useState<string>('USDT')
 
+ const handleToggleDropdown = () => {
+   setDropdownOpen(!isDropdownOpen)
+ }
+
+ const handleTokenChange = (token: string) => {
+   setSelectedToken(token)
+   setDropdownOpen(false)
+  }
+   const getTokenImage = (token: string) => {
+     switch (token) {
+       case 'USDT':
+         return <Image src={usdt} alt="USDT" className="w-6 h-6 mr-2" />
+       case 'BTC':
+         return <FaBtc size={20} className="w-6 h-6 mr-2 text-yellow-300" />
+       
+       default:
+         return null
+     }
+   }
   return (
     <main className="w-full bg-primary_13">
       <Nav />
@@ -17,19 +40,37 @@ const PresalePage = () => {
       <div className="flex flex-col gap-6  items-center mt-12 md:mt-0 font-archivo shadow-xl  ">
         <Image src={logo} alt="Decenter logo" className="w-[40%] sm:w-[20%] md:w-[15%]" />
 
-        <div className=" rounded-lg shadow bg-[rgba(5,5,5,0.80)] border border-[#232323] p-6  w-[90%] mx-auto sm:w-[60%] md:w-[50%] lg:w-[40%] xl:w-[30%] 2xl:w-[20%]">
+        <div className=" rounded-lg shadow bg-[rgba(5,5,5,0.80)] border border-[#232323] p-6  w-[90%] mx-auto sm:w-[60%] md:w-[50%] lg:w-[40%] xl:w-[30%] 2xl:w-[20%] mb-8">
           <form action="" className="flex flex-col   gap-4 ">
-            <div>
-              <div className="flex flex-col gap-1 text-sm">
-                <label htmlFor="token" className="text-sm ">
-                  <select className="p-2    rounded-lg bg-transparent focus:border-none active:border-none w-max ">
-                    <option value="USDT" className="text-black">
-                      USDT
-                    </option>
-                    <option value="BTC" className="text-black">
-                      BTC
-                    </option>
-                  </select>
+            <div className="flex flex-col ">
+              <div className="flex flex-col gap-1 text-sm relative">
+                <label htmlFor="token" className="text-sm">
+                  <div className="relative">
+                    <div
+                      className="flex items-center py-2 gap-1  rounded-lg cursor-pointer"
+                      onClick={handleToggleDropdown}>
+                      {getTokenImage(selectedToken)}
+                      <span className="text-white">{selectedToken}</span>
+                      <PiCaretDown size={20} className="text-[#8F8F8F]" />
+                    </div>
+
+                    {isDropdownOpen && (
+                      <div className="absolute top-full left-0 bg-[#2B2B2B]  rounded-lg mt-2 p-2">
+                        <div
+                          className="p-2 cursor-pointer flex gap-1 items-center"
+                          onClick={() => handleTokenChange('USDT')}>
+                          <Image src={usdt} alt="USDT" className="w-6 h-6 mr-2" />
+                          <span className="text-white">USDT</span>
+                        </div>
+                        <div
+                          className="p-2 cursor-pointer flex gap-1 items-center"
+                          onClick={() => handleTokenChange('BTC')}>
+                          <FaBtc size={20} className="w-6 h-6 mr-2 text-yellow-300" />
+                          <span className="text-white">BTC</span>
+                        </div>
+                      </div>
+                    )}
+                  </div>
                 </label>
                 <input
                   type="number"
@@ -40,7 +81,10 @@ const PresalePage = () => {
                 <LuArrowBigDown size={40} className="w-max mx-auto text-[#5D5D5D]" />
               </div>
               <div className="flex flex-col gap-1 text-sm">
-                <label htmlFor="dcen">DCEN</label>
+                <label htmlFor="dcen" className="flex items-center gap-2 pl-2 mb-1">
+                  <Image src={icon} alt="decenter icon" />
+                  DCEN
+                </label>
                 <input
                   type="number"
                   className="border  rounded-2xl border-[#2B2B2B] bg-transparent focus-within:outline-none p-3 placeholder:px-2"
@@ -56,7 +100,7 @@ const PresalePage = () => {
               Proceed
             </button>
           </form>
-          <div className="flex flex-col gap-4 text-sm mt-4">
+          <div className="flex flex-col gap-2 text-sm mt-2">
             <div className="flex  justify-between gap-4 items-center">
               <p className=" text-primary_7">Minimum Allocation</p>
               <span className="font-medium text-primary_5">$50</span>
